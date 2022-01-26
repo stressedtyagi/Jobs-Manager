bb8a0ef9f373845354d28d46790092b6eebef1e7
creatorgaming <divyanshutyagi8@gmail.com>
2022-01-26 20:12:46 +0530

    [SingIn.js] Code Refractoring

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,16 +1,18 @@
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import {
+    Avatar,
+    Button,
+    CssBaseline,
+    TextField,
+    FormControlLabel,
+    Checkbox,
+    Link,
+    Grid,
+    Box,
+    Typography,
+    Container,
+    Alert,
+} from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Alert from "@mui/material/Alert";
 import Copyright from "./Copyright";
 import axios from "axios";
 import { useState, useEffect } from "react";
