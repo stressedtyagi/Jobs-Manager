1f5491e69a017a8889fc72af7b58407cf379a954
creatorgaming <divyanshutyagi8@gmail.com>
2022-01-27 23:51:42 +0530

    dashboard UI updated

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -1,9 +1,158 @@
-const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
-};
+import { useState, useEffect } from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import { mainListItems, secondaryListItems } from "../helpers/listItems";
+import Copyright from "../components/Copyright";
+import { useOutletContext } from "react-router";
 
-export default Dashboard;
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer, {
+    shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+    "& .MuiDrawer-paper": {
+        position: "relative",
+        whiteSpace: "nowrap",
+        width: drawerWidth,
+        transition: theme.transitions.create("width", {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        boxSizing: "border-box",
+        ...(!open && {
+            overflowX: "hidden",
+            transition: theme.transitions.create("width", {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            width: theme.spacing(7),
+            [theme.breakpoints.up("sm")]: {
+                width: theme.spacing(9),
+            },
+        }),
+    },
+}));
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+    const [open, setOpen] = useState(true);
+    const [signedIn] = useOutletContext();
+
+    /**
+     * Debuging start
+     */
+    useEffect(() => {
+        console.log("DASHBOARD : " + signedIn);
+    }, [signedIn]);
+    /**
+     * Debuging end
+     */
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <>
+            {!signedIn ? (
+                ""
+            ) : (
+                <ThemeProvider theme={mdTheme}>
+                    <Box sx={{ display: "flex" }}>
+                        <CssBaseline />
+                        <Drawer variant="permanent" open={open}>
+                            <Toolbar
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "flex-end",
+                                    px: [1],
+                                }}
+                            >
+                                <IconButton onClick={toggleDrawer}>
+                                    <ChevronLeftIcon />
+                                </IconButton>
+                            </Toolbar>
+                            <Divider />
+                            <List>{mainListItems}</List>
+                            <Divider />
+                            <List>{secondaryListItems}</List>
+                        </Drawer>
+                        <Box
+                            component="main"
+                            sx={{
+                                backgroundColor: (theme) =>
+                                    theme.palette.mode === "light"
+                                        ? theme.palette.grey[100]
+                                        : theme.palette.grey[900],
+                                flexGrow: 1,
+                                height: "100vh",
+                                overflow: "auto",
+                            }}
+                        >
+                            <Toolbar />
+                            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                                <Grid container spacing={3}>
+                                    {/* Chart */}
+                                    <Grid item xs={12} md={8} lg={9}>
+                                        <Paper
+                                            sx={{
+                                                p: 2,
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                height: 240,
+                                            }}
+                                        >
+                                            COMPONENT - 1
+                                        </Paper>
+                                    </Grid>
+                                    {/* Recent Deposits */}
+                                    <Grid item xs={12} md={4} lg={3}>
+                                        <Paper
+                                            sx={{
+                                                p: 2,
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                height: 240,
+                                            }}
+                                        >
+                                            COMPONENT - 2
+                                        </Paper>
+                                    </Grid>
+                                    {/* Recent Orders */}
+                                    <Grid item xs={12}>
+                                        <Paper
+                                            sx={{
+                                                p: 2,
+                                                display: "flex",
+                                                flexDirection: "column",
+                                            }}
+                                        >
+                                            COMPONENT - 3
+                                        </Paper>
+                                    </Grid>
+                                </Grid>
+                                <Copyright sx={{ pt: 4 }} />
+                            </Container>
+                        </Box>
+                    </Box>
+                </ThemeProvider>
+            )}
+        </>
+    );
+}
+
+export default function Dashboard() {
+    return <DashboardContent />;
+}
---
+++ client/src/helpers/listItems.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PeopleIcon from "@mui/icons-material/People";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
+export const mainListItems = (
+    <div>
+        <ListItem button>
+            <ListItemIcon>
+                <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <ShoppingCartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Create Job" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Update Job" />
+        </ListItem>
+    </div>
+);
+
+export const secondaryListItems = (
+    <div>
+        <ListSubheader inset>MORE CONTENT</ListSubheader>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="RB 1" />
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="RB 2" />
+        </ListItem>
+    </div>
+);
