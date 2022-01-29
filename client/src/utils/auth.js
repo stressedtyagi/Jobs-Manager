86d7df608238bbc88d7f408a85c69d07e815aadf
creatorgaming <divyanshutyagi8@gmail.com>
2022-01-29 20:49:33 +0530

    added post route hander in auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -3,11 +3,25 @@ import axios from "axios";
 const auth = {
     get: async (url, params) => {
         const token = params?.token;
+
         return await axios.get(url, {
             headers: {
                 Authorization: token,
             },
         });
+    },
+    post: async (url, params) => {
+        const token = params?.token;
+
+        return await axios.post(
+            url,
+            {},
+            {
+                headers: {
+                    Authorization: token,
+                },
+            }
+        );
     },
 };
 
