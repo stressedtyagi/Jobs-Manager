3c247cfe8bdfda5533aafe726218e00b41d45e49
creatorgaming <divyanshutyagi8@gmail.com>
2022-01-31 20:04:27 +0530

    [Temp Addition] Drawer for toolbar

--- client/src/helpers/listItems.js
+++ client/src/helpers/listItems.js
@@ -2,11 +2,9 @@
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 import PeopleIcon from "@mui/icons-material/People";
-import AssignmentIcon from "@mui/icons-material/Assignment";
 
 export const mainListItems = (
     <div>
@@ -14,11 +12,11 @@ export const mainListItems = (
             <ListItemIcon>
                 <DashboardIcon />
             </ListItemIcon>
-            <ListItemText primary="Dashboard" />
+            <ListItemText primary="View All Jobs" />
         </ListItem>
         <ListItem button>
             <ListItemIcon>
-                <ShoppingCartIcon />
+                <AddCircleIcon />
             </ListItemIcon>
             <ListItemText primary="Create Job" />
         </ListItem>
@@ -27,24 +25,6 @@ export const mainListItems = (
                 <PeopleIcon />
             </ListItemIcon>
             <ListItemText primary="Update Job" />
-        </ListItem>
-    </div>
-);
-
-export const secondaryListItems = (
-    <div>
-        <ListSubheader inset>MORE CONTENT</ListSubheader>
-        <ListItem button>
-            <ListItemIcon>
-                <AssignmentIcon />
-            </ListItemIcon>
-            <ListItemText primary="RB 1" />
-        </ListItem>
-        <ListItem button>
-            <ListItemIcon>
-                <AssignmentIcon />
-            </ListItemIcon>
-            <ListItemText primary="RB 2" />
         </ListItem>
     </div>
 );
