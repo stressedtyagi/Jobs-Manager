58aaee71ad35ca5444e19424bd6bc27e323ab464
creatorgaming <divyanshutyagi8@gmail.com>
2022-02-09 23:50:02 +0530

    logo updated, header style update [Final]

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,95 +1,181 @@
-// mui imports
-import { Button, Chip } from "@mui/material";
+import {
+    AppBar,
+    Box,
+    Button,
+    IconButton,
+    Toolbar,
+    Typography,
+    Avatar,
+    Menu,
+    MenuItem,
+    Tooltip,
+    Chip,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-//other imports
-import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
-const myStyle = {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
-    justifyItems: "center",
-    gap: "20px",
-    fontSize: "30px",
-    fontFamily: "Roboto Mono",
-    textDecoration: "none",
-};
+import "@fontsource/roboto-mono";
+import "@fontsource/anonymous-pro";
+import "@fontsource/roboto";
 
-const Header = (props) => {
-    const [user, login, logout] = props.context;
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#1976d2",
+        },
+    },
+});
+
+function Header({ context }) {
+    const [anchorElUser, setAnchorElUser] = useState(null);
+    const [user, login, logout] = context;
     const navigate = useNavigate();
 
-    /**
-     * [SETUP] : Put login button rather than link and make drop down form for signin
-     */
-    const handleLogOut = (event) => {
-        event.preventDefault();
-
-        logout();
-        navigate("");
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
     };
 
-    /**
-     * [TODO] : UPDATE NAVBAR : Better Styling, Better Buttons
-     */
-    return (
-        <nav
-            style={{
-                borderBottom: "solid 1px #ccc",
-                paddingBottom: "10px",
-            }}
-        >
-            <div style={myStyle}>
-                <NavLink
-                    to="/"
-                    style={({ isActive }) => ({
-                        color: isActive ? "red" : "",
-                    })}
-                >
-                    Home
-                </NavLink>
+    const handleCloseUserMenu = (setting) => {
+        if (setting.toLowerCase() === "logout") {
+            logout();
+        } else {
+            navigate(`/${setting.toLowerCase()}`);
+        }
+        setAnchorElUser(null);
+    };
 
-                {user ? (
-                    <>
-                        <NavLink
-                            to="/dashboard"
-                            style={({ isActive }) => ({
-                                color: isActive ? "red" : "",
-                            })}
+    const handleCloseUserMenuMain = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <ThemeProvider theme={darkTheme}>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            onClick={() => navigate("/")}
                         >
-                            Dashboard
-                        </NavLink>
-                        <Chip label={`Hello ${user.name}`} variant="outlined" />
-                        <Button
-                            variant="contained"
-                            style={{ justifySelf: "end", marginTop: "5px" }}
-                            onClick={handleLogOut}
+                            <Avatar
+                                alt="Remy Sharp"
+                                src="/assets/logo-white.png"
+                                sx={{ height: 35, width: 35 }}
+                            />
+                        </IconButton>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: {
+                                    xs: "none",
+                                    md: "flex",
+                                    justifyContent: "center",
+                                },
+                            }}
                         >
-                            Logout
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        <NavLink
-                            to="/signup"
-                            style={({ isActive }) => ({
-                                color: isActive ? "red" : "",
-                            })}
-                        >
-                            Sign Up
-                        </NavLink>
-                        <NavLink
-                            to="/signin"
-                            style={({ isActive }) => ({
-                                color: isActive ? "red" : "",
-                            })}
-                        >
-                            Sign In
-                        </NavLink>
-                    </>
-                )}
-            </div>
-        </nav>
+                            <Typography
+                                variant="h6"
+                                component="div"
+                                sx={{
+                                    fontSize: 30,
+                                    fontWeight: "bold",
+                                    fontFamily: "Roboto Mono",
+                                }}
+                            >
+                                Jobify
+                            </Typography>
+                        </Box>
+
+                        <Box sx={{ flexGrow: 0 }}>
+                            {user ? (
+                                <>
+                                    <Tooltip title="Open settings">
+                                        <IconButton
+                                            onClick={handleOpenUserMenu}
+                                            sx={{ p: 0 }}
+                                        >
+                                            {/* [TODO] Change src to user image */}
+                                            {/* [TODO] Setup random bgcolor for each user */}
+                                            {/* Change alt text to user state name */}
+                                            <Avatar
+                                                alt={user.name}
+                                                // src="/static/images/avatar/2.jpg"
+                                                children={
+                                                    user.name.toUpperCase()[0] +
+                                                    user.name.toUpperCase()[1]
+                                                }
+                                            />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Menu
+                                        sx={{ mt: "45px" }}
+                                        id="menu-appbar"
+                                        anchorEl={anchorElUser}
+                                        anchorOrigin={{
+                                            vertical: "top",
+                                            horizontal: "right",
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: "top",
+                                            horizontal: "right",
+                                        }}
+                                        open={Boolean(anchorElUser)}
+                                        onClose={handleCloseUserMenuMain}
+                                    >
+                                        <MenuItem>
+                                            <Chip
+                                                key="uname-chip"
+                                                label={`${user.name.toUpperCase()}`}
+                                            />
+                                        </MenuItem>
+                                        {settings.map((setting) => (
+                                            <MenuItem
+                                                key={setting}
+                                                onClick={handleCloseUserMenu.bind(
+                                                    this,
+                                                    setting
+                                                )}
+                                            >
+                                                <Typography textAlign="center">
+                                                    {setting}
+                                                </Typography>
+                                            </MenuItem>
+                                        ))}
+                                    </Menu>
+                                </>
+                            ) : (
+                                <>
+                                    <Button
+                                        variant="contained"
+                                        sx={{ mr: 2 }}
+                                        onClick={() => navigate("/signin")}
+                                    >
+                                        SignIn
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        onClick={() => navigate("/signup")}
+                                    >
+                                        SignUp
+                                    </Button>
+                                </>
+                            )}
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            </ThemeProvider>
+        </Box>
     );
-};
+}
 
 export default Header;
