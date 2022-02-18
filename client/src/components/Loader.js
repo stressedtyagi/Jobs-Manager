42183793a44f3b89272d0320bb12fda37bb890af
creatorgaming <divyanshutyagi8@gmail.com>
2022-02-18 21:46:42 +0530

    minor changes in server and client

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,12 +2,7 @@ import { Backdrop, CircularProgress, Stack } from "@mui/material";
 
 const Loader = (props) => {
     return (
-        <Stack
-            alignItems="center"
-            // border="10px solid black"
-            height="100vh"
-            justifyContent="center"
-        >
+        <Stack alignItems="center" height="100vh" justifyContent="center">
             <Backdrop open={true} sx={{ color: "#fff" }}>
                 <CircularProgress size={80} {...props} />
             </Backdrop>
